e9842e3b29d4af822a4aadbff4dbbb7a5c940ff4
RCorleto1 <2024646@student.cct.ie>
2024-12-31 01:37:13 +0000

    Add files via upload

---
+++ src/git_2024646/Git_2024646.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024646;
+
+/**
+ *
+ * @author rorro
+ */
+public class Git_2024646 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Rodrigo Corleto 2024646");
+    }
+    
+}
